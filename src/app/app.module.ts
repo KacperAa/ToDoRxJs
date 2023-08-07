7d0e119251146f22092aec7895951b390e882b69
KacperAa <kacperaugvstyn@gmail.com>
2023-08-07 11:52:19 +0200

    patch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
-import { HeroComponent } from './components/list/hero/hero.component';
+import { HeroComponent } from './components/hero/hero.component';
 import { ButtonToggleComponent } from './UI/button-toggle/button-toggle.component';
-import { TasksListComponent } from './UI/button-toggle/tasks-list/tasks-list.component';
-import { CheckboxComponent } from './UI/button-toggle/checkbox/checkbox.component';
+import { TasksListComponent } from './UI/tasks-list/tasks-list.component';
+import { CheckboxComponent } from './UI/checkbox/checkbox.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
