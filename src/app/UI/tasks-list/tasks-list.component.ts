8230b326d2230cd91c5693b94f6975bd9276ae6d
KacperAa <kacperaugvstyn@gmail.com>
2023-08-08 11:26:26 +0200

    patch

--- src/app/UI/tasks-list/tasks-list.component.ts
+++ src/app/UI/tasks-list/tasks-list.component.ts
@@ -18,67 +18,62 @@ export class TasksListComponent implements AfterViewInit {
     this.draggable();
   }
 
-  public draggable() {
-    // (A) SET CSS + GET ALL LIST ITEMS
-    const target = document.getElementById('sortList');
+  public draggable(): void {
+    const sortList = document.getElementById('sortList');
 
-    let items: any = target?.getElementsByTagName('li');
-    let current: HTMLLIElement | null = null;
+    let liCollection: any = sortList?.getElementsByTagName('li');
+    let selectedLi: HTMLLIElement | null = null;
 
     // (B) MAKE ITEMS DRAGGABLE + SORTABLE
-    for (let i of items) {
-      // (B1) ATTACH DRAGGABLE
-      i.draggable = true;
+    for (let li of liCollection) {
+      li.ondragstart = () => {
+        selectedLi = li;
 
-      // (B2) DRAG START - YELLOW HIGHLIGHT DROPZONES
-      i.ondragstart = (e: any) => {
-        current = i;
-        for (let it of items) {
-          if (it != current) {
-            it.classList.add('hint');
+        for (let otherLi of liCollection) {
+          if (otherLi != selectedLi) {
+            otherLi.classList.add('hint');
           }
         }
       };
 
-      // (B3) DRAG ENTER - RED HIGHLIGHT DROPZONE
-      i.ondragenter = (e: any) => {
-        if (i != current) {
-          i.classList.add('active');
+      li.ondragenter = () => {
+        if (li !== selectedLi) {
+          li.classList.add('active');
         }
       };
 
       // (B4) DRAG LEAVE - REMOVE RED HIGHLIGHT
-      i.ondragleave = () => i.classList.remove('active');
+      li.ondragleave = () => li.classList.remove('active');
 
       // (B5) DRAG END - REMOVE ALL HIGHLIGHTS
-      i.ondragend = () => {
-        for (let it of items) {
-          it.classList.remove('hint');
-          it.classList.remove('active');
+      li.ondragend = () => {
+        for (let li of liCollection) {
+          li.classList.remove('hint');
+          li.classList.remove('active');
         }
       };
 
       // (B6) DRAG OVER - PREVENT THE DEFAULT "DROP", SO WE CAN DO OUR OWN
-      i.ondragover = (e: any) => e.preventDefault();
+      li.ondragover = (selectedLi: DragEvent) => selectedLi.preventDefault();
 
       // (B7) ON DROP - DO SOMETHING
-      i.ondrop = (e: any) => {
-        e.preventDefault();
-        if (i != current) {
-          let currentpos = 0,
-            droppedpos = 0;
-          for (let it = 0; it < items.length; it++) {
-            if (current == items[it]) {
-              currentpos = it;
+      li.ondrop = (movedLi: DragEvent) => {
+        movedLi.preventDefault();
+        if (li !== selectedLi) {
+          let actualPos = 0;
+          let droppedPos = 0;
+          for (let li = 0; li < liCollection.length; li++) {
+            if (selectedLi === liCollection[li]) {
+              actualPos = li;
             }
-            if (i == items[it]) {
-              droppedpos = it;
+            if (li === liCollection[li]) {
+              droppedPos = li;
             }
           }
-          if (currentpos < droppedpos) {
-            i.parentNode.insertBefore(current, i.nextSibling);
+          if (actualPos < droppedPos) {
+            li.parentNode.insertBefore(selectedLi, li.nextSibling);
           } else {
-            i.parentNode.insertBefore(current, i);
+            li.parentNode.insertBefore(selectedLi, li);
           }
         }
       };
