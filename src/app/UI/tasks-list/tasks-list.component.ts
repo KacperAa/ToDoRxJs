16298ddbf0256633142895630ce315a131304e1c
KacperAa <kacperaugvstyn@gmail.com>
2023-08-09 12:27:05 +0200

    patch

--- src/app/UI/tasks-list/tasks-list.component.ts
+++ src/app/UI/tasks-list/tasks-list.component.ts
@@ -1,7 +1,8 @@
 import { AfterViewInit, Component, Input } from '@angular/core';
-import { Subject, exhaustMap } from 'rxjs';
+import { Subject, exhaustMap, map, of } from 'rxjs';
 import { dragAndDrop } from 'src/app/functions/drag-and-drop.function';
 import { List } from 'src/app/models/list-item.interface';
+import { Task } from 'src/app/models/task.interface';
 import { TasksService } from 'src/app/services/tasks.service';
 
 @Component({
@@ -16,7 +17,20 @@ export class TasksListComponent implements AfterViewInit {
   public isLightMode!: boolean;
   public taskId$ = new Subject<string>();
 
-  public handleCheckbox(checked: boolean) {}
+  public handleCheckbox(checked: boolean, task: Task): void {
+    this.list.listItems
+      ?.pipe(
+        map((tasks: Task[]) => {
+          const taskEl = tasks.find((listEl: Task) => listEl.id === task.id);
+          taskEl!.active = checked;
+          return tasks;
+        })
+      )
+      .subscribe((newList: Task[]) => {
+        const newList$ = of(newList);
+        this.list.listItems = newList$;
+      });
+  }
 
   constructor(private _tasksService: TasksService) {}
 
